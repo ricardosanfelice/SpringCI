84ceebc668d7a9bd08a7e076ebc3b5d3d791614e
Ricardo Sanfelice <ricardo.sanfelice@vrsoft.com.br>
2020-10-08 14:01:09 -0300

    Added validation
    Added null and empty validations to `DateUtils.toLocalDate` function

--- src/main/java/br/com/sanfelice/javautils/utils/date/DateUtils.java
+++ src/main/java/br/com/sanfelice/javautils/utils/date/DateUtils.java
@@ -3,12 +3,16 @@ package br.com.sanfelice.javautils.utils.date;
 import java.time.LocalDate;
 
 public class DateUtils {
-    
+
     private DateUtils() {
-        
+
     }
 
     public static LocalDate toLocalDate(String strDate) {
+        if (strDate == null || strDate.isBlank()) {
+            return null;
+        }
+
         return LocalDate.parse(strDate);
     }
 }
