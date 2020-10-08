6bf59f8af97af71f92e071b005fae2b6f84f3415
Ricardo Sanfelice <71078825+ricardosanfelice@users.noreply.github.com>
2020-10-08 14:04:23 -0300

    Added validation (#2)
    Added null and empty validations to `DateUtils.toLocalDate` function

--- src/test/java/br/com/sanfelice/javautils/utils/date/DateUtilsTest.java
+++ src/test/java/br/com/sanfelice/javautils/utils/date/DateUtilsTest.java
@@ -1,12 +1,37 @@
 package br.com.sanfelice.javautils.utils.date;
 
 import java.time.LocalDate;
-
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class DateUtilsTest {
+
+    @Test
+    @DisplayName("Must return null on null parameter")
+    public void mustReturnNullOnNull() {
+        LocalDate actual = DateUtils.toLocalDate(null);
+
+        assertThat(actual, nullValue());
+    }
+
+    @Test
+    @DisplayName("Must return null on empty parameter")
+    public void mustReturnNullOnEmpty() {
+        LocalDate actual = DateUtils.toLocalDate("");
+
+        assertThat(actual, nullValue());
+    }
+
+    @Test
+    @DisplayName("Must return null on blank parameter")
+    public void mustReturnNullOnBlank() {
+        LocalDate actual = DateUtils.toLocalDate("   ");
+
+        assertThat(actual, nullValue());
+    }
 
     @Test
     public void mustConvertStringToLocalDate() {
